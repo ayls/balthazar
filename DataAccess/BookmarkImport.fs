9bfca5f72a00fc9fcd5c74038459913b0cf704f0
ayls <ales.daneu@gmail.com>
2020-04-11 19:47:48 +0200

    Update with the rest of the DataAccess functions

---
+++ DataAccess/BookmarkImport.fs
@@ -0,0 +1,36 @@
+﻿namespace DataAccess
+
+open System
+open System.IO
+open System.Linq
+open BookmarksManager
+open BookmarkStorage
+open Microsoft.WindowsAzure.Storage.Table
+
+module BookmarkImport =
+    let private readExport path =
+        use file = File.OpenRead(path)
+        let reader = new NetscapeBookmarksReader()
+        reader.Read(file)
+
+    let rec importExport (table:CloudTable) (bookmarkCollectionId: string) (bookmarkItem:IBookmarkItem) (parentBookmarkItem:IBookmarkFolder) =
+        let recordId = bookmarkItem.GetHashCode().ToString()
+        let parentRecordId = 
+            match parentBookmarkItem with
+            | null -> ""
+            | _ -> parentBookmarkItem.GetHashCode().ToString()
+        match bookmarkItem with
+        | :? IBookmarkFolder as folder -> new BookmarkRecord(bookmarkCollectionId, recordId, parentRecordId, folder.Title, "", true) |> BookmarkStorage.insert table
+        | :? IBookmarkLink as link -> new BookmarkRecord(bookmarkCollectionId, recordId, parentRecordId, link.Title, link.Url, false) |> BookmarkStorage.insert table          
+        | _ -> ()
+        match bookmarkItem with
+        | :? IBookmarkFolder as folder -> for item in folder do importExport table bookmarkCollectionId item folder
+        | _ -> ()
+
+    let import path connectionString = 
+        let table = openTable connectionString
+        let bookmarkCollectionId = Guid.NewGuid().ToString()
+        let bookmarkExport = readExport path
+        let rootBookmarkFolder = bookmarkExport.First()
+        importExport table bookmarkCollectionId rootBookmarkFolder null
+
---
+++ DataAccess/BookmarkStorage.fs
@@ -0,0 +1,62 @@
+﻿namespace DataAccess
+
+open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Table
+
+module BookmarkStorage =
+    type BookmarkRecord(bookmarkCollectionId: string, recordId: string, parentRowKey: string, name: string, url: string, isFolder: bool) =
+        inherit TableEntity(partitionKey=bookmarkCollectionId, rowKey=recordId)
+        new() = BookmarkRecord(null, null, null, null, null, false)
+        member val ParentRowKey = parentRowKey with get, set
+        member val Name = name with get, set
+        member val Url = url with get, set
+        member val IsFolder = isFolder with get, set
+
+    let openTable (connectionString: string) = 
+        let storageAccount = CloudStorageAccount.Parse(connectionString)
+        let tableClient = storageAccount.CreateCloudTableClient()
+        let table = tableClient.GetTableReference("Bookmarks")        
+        table.CreateIfNotExistsAsync() 
+        |> Async.AwaitTask 
+        |> Async.RunSynchronously 
+        |> ignore
+        table
+
+    let insert (table:CloudTable) (bookmarkRecord:BookmarkRecord) =
+        let insertOp = TableOperation.Insert(bookmarkRecord)        
+        table.ExecuteAsync(insertOp)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+        |> ignore
+
+    let update (table:CloudTable) (bookmarkRecord:BookmarkRecord) =
+        let replaceOp = TableOperation.Replace(bookmarkRecord)      
+        table.ExecuteAsync(replaceOp)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+        |> ignore
+
+    let delete (table:CloudTable) (bookmarkRecord:BookmarkRecord) =
+        let deleteOp = TableOperation.Delete(bookmarkRecord)        
+        table.ExecuteAsync(deleteOp)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+        |> ignore
+
+    let list (table:CloudTable) (bookmarkCollectionId: string) =
+        let bookmarks = new System.Collections.Generic.List<BookmarkRecord>();
+        let query =
+            TableQuery<BookmarkRecord>().Where(
+                TableQuery.GenerateFilterCondition(
+                    "PartitionKey", QueryComparisons.Equal, bookmarkCollectionId))
+        let asyncQuery = 
+            let rec loop (cont: TableContinuationToken) = async {
+                let! result = table.ExecuteQuerySegmentedAsync(query, cont) |> Async.AwaitTask
+                bookmarks.AddRange(result)
+                match result.ContinuationToken with
+                | null -> ()
+                | cont -> return! loop cont
+            }
+            loop null
+        asyncQuery |> Async.RunSynchronously
+        bookmarks
