f54be406f58dc28514e061a2010fc365f2d398e4
ayls <ales.daneu@gmail.com>
2020-09-13 12:07:26 +0200

    Correct runtime config

--- Web/plugins/auth.js
+++ Web/plugins/auth.js
@@ -1,6 +1,6 @@
 export default function ({ $auth }) {
   if (window.config) {
-    $auth.strategies.oAuth2.options.authorization_endpoint = window.config.authorization_endpoint;
-    $auth.strategies.oAuth2.options.client_id = window.config.authorization_client_id;
+    $auth.strategies.oAuth2.options.endpoints.authorization = window.config.authorization_endpoint;
+    $auth.strategies.oAuth2.options.clientId = window.config.authorization_client_id;
   }
 }
